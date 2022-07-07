867a01e9ff246d7df11a10f5d6a1b3d65ddd8f9c
mdbappa0001 <abutanif366@gmail.com>
2022-07-08 05:29:53 +0600

    requireAuth added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Pages/Blogs/Blogs';
+import CheckOut from './Pages/CheckOut/CheckOut';
 import Home from './Pages/Home/Home';
 import Portfolio from './Pages/Portfolio/Portfolio';
 import Navbar from './Shared/Navbar';
+import RequireAuth from './Shared/RequireAuth';
 
 function App() {
   return (
@@ -13,6 +15,12 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
+        <Route path='/checkout' element=
+          {
+            <RequireAuth>
+              <CheckOut></CheckOut>
+            </RequireAuth>
+          }></Route>
       </Routes>
     </>
   );
